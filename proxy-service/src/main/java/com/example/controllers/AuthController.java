6896399ec48d3c05b38f67cde6b5121906485405
ghassenbaklouti <ghassenbaklouti@yahoo.com>
2021-01-21 11:20:32 +0100

    get user by email added

--- proxy-service/src/main/java/com/example/controllers/AuthController.java
+++ proxy-service/src/main/java/com/example/controllers/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -53,6 +54,11 @@ public class AuthController {
 
 	@Autowired
 	JwtUtils jwtUtils;
+	
+	@GetMapping(value="user/email/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return this.userRepository.findByUsername(email).get();
+    }
 
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
